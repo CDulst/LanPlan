807dc8da2a57dde9f9913bab84e9fea6568b8f25
yorbenGoor123 <yorbengoor5@gmail.com>
2019-11-17 00:13:19 +0100

    Responsive CSS 768, 1200px -> Add/change Name closes #41

--- src/view/layout.php
+++ src/view/layout.php
@@ -24,9 +24,9 @@
 
     <header class="header">
         <h1 class="header__title">Lan plan</h1>
-        <div class="header__image__container1"><img class="header__image1" src="../assets/images/circle_object.svg" alt="circle_object"></div>
-        <div class="header__image__container2"><img class="header__image2" src="../assets/images/circle_object.svg" alt="circle_object"></div>
-        <div class="header__image__container3"><img class="header__image3" src="../assets/images/circle_object.svg" alt="circle_object"></div>
+        <div class="header__image__container1"><img class="header__image1" src="../src/assets/images/circle_object.svg" alt="circle_object"></div>
+        <div class="header__image__container2"><img class="header__image2" src="../src/assets/images/circle_object.svg" alt="circle_object"></div>
+        <div class="header__image__container3"><img class="header__image3" src="../src/assets/images/circle_object.svg" alt="circle_object"></div>
     </header>
 
     <?php echo $content;?>
