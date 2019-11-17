42a440a2964927692ab74a12e613f67624853b19
yorbenGoor123 <yorbengoor5@gmail.com>
2019-11-17 13:56:59 +0100

    Bug fixes responsive

--- src/view/layout.php
+++ src/view/layout.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta charset="utf-8">
     <title>LanPlan</title>
     <?php /* NEW */ ?>
@@ -39,4 +40,6 @@
     </div>
   </body>
   <div class="body__wrapper">
+
+
 </html>
